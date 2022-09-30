9ac5ec1407a8de5b90f47810e85907c9e7ae7335
Julian <al_jdomenecht@iessierradelasvillas.org>
2022-09-30 11:14:04 +0200

    Arreglado

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -2,4 +2,4 @@
 import math
 cateto1=(int)(input("Inque el primer cateto"))
 cateto2=(int)(input("Inque el segundo cateto"))
-print("El área del triangulo es",(cateto1^2+cateto2^2))
+print("El área del triangulo es",math.sqrt(cateto1*cateto1+cateto2*cateto2))
