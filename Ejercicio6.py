5fa5340c5a1ab88fa4c8868a6aef9b6b03c9ce6c
Julian <al_jdomenecht@iessierradelasvillas.org>
2022-10-03 10:55:03 +0200

    Ejercicios 6 y 7 hechos

---
+++ Ejercicio6.py
@@ -0,0 +1,4 @@
+num1=int (input ("Dime el primer numero de la operación "))
+num2=int (input ("Dime el segundo numero de la operación "))
+num3=int (input ("Dime el tercer numero de la operación "))
+print("La media de los núeros es",(num1+num2+num3)/3)
---
+++ Ejercicio7.py
@@ -0,0 +1,2 @@
+min=int(input("Dime la cantidad de mimnutos que quieres convertir a horas"))
+print("La conersion a horas es",int(min/60),"y",min%60, "minutos")
